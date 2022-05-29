1bc897e4b1ae61beb8be566a4efdbda2911c0a0d
mncolla <maxincolla@gmail.com>
2022-05-29 05:06:30 -0300

    creo nuevos enpoints para forecast

--- routes/v1/forecast/index.js
+++ routes/v1/forecast/index.js
@@ -1,5 +1,23 @@
+import OpenWeatherMap from '../../../services/openWeatherMap.js';
+import IpApi from '../../../services/ipApi.js';
+
 export default async (fastify, opts) => {
+	fastify.get('/:city', async (request, reply) => {
+		const owm = new OpenWeatherMap('forecast');
+		owm.setCity(request.params.city);
+		const forecast = await owm.execute();
+		return forecast;
+	});
+
 	fastify.get('/', async (request, reply) => {
-		return 'Forecast';
+		const ipapi = new IpApi(request);
+		const location = await ipapi.execute();
+		const owm = new OpenWeatherMap('forecast');
+		owm.setCoords(location.lat, location.lon);
+		const forecast = await owm.execute();
+		return {
+			location,
+			forecast,
+		};
 	});
 };
