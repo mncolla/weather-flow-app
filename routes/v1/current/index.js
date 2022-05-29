f49aa822c0b7afc694663a2ca33c1f1132ae699d
mncolla <maxincolla@gmail.com>
2022-05-29 03:18:54 -0300

    completo funcionalidad para enpoint current

--- routes/v1/current/index.js
+++ routes/v1/current/index.js
@@ -1,5 +1,28 @@
+import {
+	getForecastByCity,
+	getWeatherByCity,
+} from '../../../services/openWeatherMap.js';
+import { getLocation } from '../../../services/ipApi.js';
+
 export default async (fastify, opts) => {
+	fastify.get('/:city', async (request, reply) => {
+		const location = await getLocation('186.57.170.113');
+		return location;
+	});
+
 	fastify.get('/', async (request, reply) => {
-		return 'Current';
+		const ip = (
+			request.headers['x-forwarded-for'] ||
+			request.socket.remoteAddress ||
+			''
+		)
+			.split(',')[0]
+			.trim();
+		const location = await getLocation('186.57.170.113');
+		const weather = await getWeatherByCity(location.city);
+		return {
+			location,
+			weather,
+		};
 	});
 };
