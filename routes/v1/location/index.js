6a8048153735e082da69209c812b9c907a779f55
mncolla <maxincolla@gmail.com>
2022-05-29 03:18:30 -0300

    completo funcionalidad para enpoint location

--- routes/v1/location/index.js
+++ routes/v1/location/index.js
@@ -2,8 +2,15 @@ import { getLocation } from '../../../services/ipApi.js';
 
 export default async (fastify, opts) => {
 	fastify.get('/', async (request, reply) => {
-		return getLocation(
-			request.headers['x-forwarded-for'] || request.connection.remoteAddress
-		);
+		const ip = (
+			request.headers['x-forwarded-for'] ||
+			request.socket.remoteAddress ||
+			''
+		)
+			.split(',')[0]
+			.trim();
+		// TODO: Enviar ip en getLocation
+		const location = await getLocation('186.57.170.113');
+		return location;
 	});
 };
