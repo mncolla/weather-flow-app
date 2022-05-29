0a04ad5045b93f1b67cdcc8818606864846e2939
mncolla <maxincolla@gmail.com>
2022-05-29 05:06:59 -0300

    implemento nueva estructura de servicios

--- routes/v1/location/index.js
+++ routes/v1/location/index.js
@@ -1,16 +1,10 @@
-import { getLocation } from '../../../services/ipApi.js';
+import IpApi from '../../../services/ipApi.js';
 
 export default async (fastify, opts) => {
 	fastify.get('/', async (request, reply) => {
-		const ip = (
-			request.headers['x-forwarded-for'] ||
-			request.socket.remoteAddress ||
-			''
-		)
-			.split(',')[0]
-			.trim();
-		// TODO: Enviar ip en getLocation
-		const location = await getLocation('186.57.170.113');
+		const ipapi = new IpApi(request);
+		const location = ipapi.execute();
+
 		return location;
 	});
 };
